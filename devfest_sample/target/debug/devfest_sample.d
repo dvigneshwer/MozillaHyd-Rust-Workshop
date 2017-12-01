1b2b61b27898b677fa20aea215d85432e2bc32a0
Vigneshwer.D <dvigneshwer@gmail.com>
2017-12-02 01:29:51 +0530

    Rust Hyd workshop material

---
+++ devfest_sample/target/debug/devfest_sample.d
@@ -0,0 +1,1 @@
+/home/viki/Documents/events/DevfestYangon/Rust_Workshop/devfest_sample/target/debug/devfest_sample: /home/viki/Documents/events/DevfestYangon/Rust_Workshop/devfest_sample/src/main.rs
---
+++ devfest_sample/target/release/devfest_sample.d
@@ -0,0 +1,1 @@
+/home/viki/Documents/events/DevfestYangon/Rust_Workshop/devfest_sample/target/release/devfest_sample: /home/viki/Documents/events/DevfestYangon/Rust_Workshop/devfest_sample/src/main.rs
